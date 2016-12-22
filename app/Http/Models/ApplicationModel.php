e5ed950ba30a621ec4d94ab8d96cf3ce417cbdea
phuong.nd@chiroro.com.vn <phuong.nd@chiroro.com.vn>
2016-12-22 15:48:58 +0700

    update

---
+++ app/Http/Models/ApplicationModel.php
@@ -0,0 +1,18 @@
+<?php namespace App\Http\Models;
+
+class ApplicationModel
+{
+
+    protected $table = false;
+
+    //Application rules
+    public function rules(){
+        return array();
+    }
+
+    //Application messages
+    public function messages(){
+        return array();
+    }
+
+}
---
+++ app/Http/Models/ContactModel.php
@@ -0,0 +1,18 @@
+<?php namespace App\Http\Models;
+
+class ContactModel
+{
+
+    protected $table = false;
+
+    //Contact rules
+    public function rules(){
+        return array();
+    }
+
+    //Contact messages
+    public function messages(){
+        return array();
+    }
+
+}
